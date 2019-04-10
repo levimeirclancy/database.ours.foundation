93e3527c94ce551ae9d6403c19e5b52ac2df3fda
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2019-04-10 14:13:39 +0900

    Update theme_page.php

--- theme_page.php
+++ theme_page.php
@@ -12,7 +12,7 @@ foreach ($result as $row):
 if (!(empty($messenger_bot)) && file_exists("messenger/".$entry_info['entry_id'].".png")):
 	echo "<div id='messenger-code-image' ". $layout_nodisplay_temp .">";
 	echo "<a href='http://m.me/".$messenger_bot."?ref=entry_id=".$page_temp."' target='_blank'><amp-img src='/messenger/".$entry_info['entry_id'].".png' width='200px' height='200px'></amp-img></a></div>";
-	echo "<a href='/".$page_temp."/flyer/' target='_blank'><div id='messenger-flyer-button'>Get flyer</div></a>";
+	echo "<a href='/".$page_temp."/flyer/' target='_blank'><div id='messenger-flyer-button' ". $layout_nodisplay_temp .">Get flyer</div></a>";
 	endif;
 
 echo "<article><div vocab='http://schema.org/' typeof='Article'>";
