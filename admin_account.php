a105c75ce6c417e214c9f3cecaca6eb69fcfbe39
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2019-12-18 20:52:48 +0300

    Update admin_account.php

--- admin_account.php
+++ admin_account.php
@@ -1,2 +1,9 @@
-<? echo "<div class='account_button'><a href='/'><i class='material-icons'>home</i></a></div>";
-echo "<div class='command_button'><a href='/logout/'><i class='material-icons'>cancel</i></a></div>"; ?>
+<?
+
+echo "More options will be available soon.";
+
+// Change password
+
+// Manage other users
+
+?>
