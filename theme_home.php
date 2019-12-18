18cf29834d159e4e0d6ccae08973d1a1c9dc4ebb
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2019-12-18 14:22:58 +0300

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -1,5 +1,36 @@
-<? echo "<article>";
-echo "<p>site info -- welcome to $domain</p>";
-echo "</article>";
+<? echo "<br><br><br><br><p>Welcome to $domain</p>";
+
+
+$result_temp = file_get_contents("https://".$domain."/api/sitemap/?order=english");
+$information_array = json_decode($result_temp, true);
+
+// If there is nothing to proceed with ...
+if (empty($information_array)): footer(); endif;
+
+echo "<p>";
+
+// How many total entries are there ...
+echo "<b>". number_format(count($information_array)) ." total entries.</b><br>";
+
+$type_counts_array = [];
+$coordinate_counts = 0;
+foreach ($information_array as $entry_id => $entry_info):
+  if (empty($type_counts_array[$entry_info['type']])): $type_counts_array[$entry_info['type']] = 0; endif;
+	$type_counts_array[$entry_info['type']]++;
+	if (empty($entry_info['appendix']['latitude']) || empty($entry_info['appendix']['longitude'])): continue; endif;
+	$coordinate_counts++;
+	endforeach;
+
+foreach ($header_array as $header_backend => $header_frontend):
+	if (empty($type_counts_array[$header_backend])): continue; endif;
+	echo $header_frontend ."— ".number_format($type_counts_array[$header_backend])." entries.<br>";
+	endforeach;
+
+// Display how many have GPS coordinates ...
+if (!(empty($coordinate_counts)): echo number_format($coordinate_counts)." entries with GPS coordinates.<br>"; endif;
+
+echo "</p>";
+		
+// Show latest edits
 
 footer(); ?>
