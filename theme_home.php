980d43ab0afe0115d04eac9325093cf916e13f11
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-07 20:05:57 -0700

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -1,4 +1,6 @@
-<? echo "<amp-carousel type='slides' width='450' heifght='300' controls loop autoplay delay='2000'>";
+<? echo "<br><br><br>";
+
+echo "<amp-carousel type='slides' width='450' heifght='300' controls loop autoplay delay='2000'>";
 
 echo "<div>Test1</div>";
 
