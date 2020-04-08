fdb7684a63b50b3c4fb2bf4bfed1aa060b10743f
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-07 21:22:11 -0700

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -2,12 +2,19 @@
 
 echo "<amp-carousel type='slides' width='450' heifght='300' loop autoplay delay='2000'>";
 
-echo "<div width='450' heifght='300'>Test1</div>";
+echo "<span>".count($information_array)." entries</span>";
 
-echo "<div width='450' heifght='300'>Test2</div>";
+	foreach ($header_array as $header_backend => $header_frontend):
+		
+		if (empty($type_counts_array[$header_backend])): continue; endif;
 
-echo "<div width='450' heifght='300'>Test3</div>";
+		echo "<amp-lightbox class='categories-list-popover-thread' id='categories-list-popover-thread-".$header_backend."' on='lightboxClose:categories-popover-close.show;lightboxOpen:categories-popover-close.hide' layout='nodisplay' scrollable>";
 
-echo "<div width='450' heifght='300'>Test4</div>";
+			echo "<div role='button' tabindex='0' on='tap:categories-list-popover-thread-".$header_backend.".close' class='popover-close'>Back</div>";	
+	
+			echo "<p>".number_format($type_counts_array[$header_backend])." ".$header_frontend."</p>";
+	
+
+echo "<span>".count($information_array)." entries</span>";
 
 echo "</amp-carousel>"; ?>
