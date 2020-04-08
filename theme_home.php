b453ec5c4b55d23e0ac45caf4f52735d30853c2d
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-07 21:28:55 -0700

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -4,17 +4,13 @@ echo "<amp-carousel type='slides' width='450' heifght='300' loop autoplay delay=
 
 echo "<span>".count($information_array)." entries</span>";
 
-	foreach ($header_array as $header_backend => $header_frontend):
-		
-		if (empty($type_counts_array[$header_backend])): continue; endif;
+foreach ($header_array as $header_backend => $header_frontend):
+	if (empty($type_counts_array[$header_backend])): continue; endif;
+	echo "<span>".$type_counts_array[$header_backend]." ".$header_frontend."</span>";
+	endforeach;
 
-		echo "<amp-lightbox class='categories-list-popover-thread' id='categories-list-popover-thread-".$header_backend."' on='lightboxClose:categories-popover-close.show;lightboxOpen:categories-popover-close.hide' layout='nodisplay' scrollable>";
-
-			echo "<div role='button' tabindex='0' on='tap:categories-list-popover-thread-".$header_backend.".close' class='popover-close'>Back</div>";	
-	
-			echo "<p>".number_format($type_counts_array[$header_backend])." ".$header_frontend."</p>";
-	
-
-echo "<span>".count($information_array)." entries</span>";
+if (!(empty($coordinate_counts))):
+	echo "<span>".$coordinate_counts." map points</span>";
+	endif;
 
 echo "</amp-carousel>"; ?>
