e63dd8196808a6990fb01b90f3d0d31c653b4999
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-07 19:38:31 -0700

    Create theme_home.php

---
+++ theme_home.php
@@ -0,0 +1,11 @@
+echo "<amp-carousel type='slides' width='450' heifght='300' controls loop autoplay delay='2000'>";
+
+echo "<div>Test1</div>";
+
+echo "<div>Test2</div>";
+
+echo "<div>Test3</div>";
+
+echo "<div>Test4</div>";
+
+echo "</amp-carousel>";
