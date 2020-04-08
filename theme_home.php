704cf2e3407433506e734fbe2f496669cf836361
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-07 21:34:04 -0700

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -2,15 +2,15 @@
 
 echo "<amp-carousel id='home-carousel' type='slides' width='450' heifght='300' layout='fixed-height' loop autoplay delay='2000'>";
 
-echo "<span>".count($information_array)." entries</span>";
+echo "<span class='home-carousel-slide'>".count($information_array)." entries</span>";
 
 foreach ($header_array as $header_backend => $header_frontend):
 	if (empty($type_counts_array[$header_backend])): continue; endif;
-	echo "<span>".$type_counts_array[$header_backend]." ".$header_frontend."</span>";
+	echo "<span class='home-carousel-slide'>".$type_counts_array[$header_backend]." ".$header_frontend."</span>";
 	endforeach;
 
 if (!(empty($coordinate_counts))):
-	echo "<span>".$coordinate_counts." map points</span>";
+	echo "<span class='home-carousel-slide'>".$coordinate_counts." map points</span>";
 	endif;
 
 echo "</amp-carousel>"; ?>
