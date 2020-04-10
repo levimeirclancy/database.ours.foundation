9c0ace7ad31964e5b650cb6657e841edf822f6d6
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-10 12:47:22 -0700

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -178,7 +178,7 @@ function amp_header($title=null, $canonical=null) {
 
 	echo "<amp-lightbox class='sidebar-navigation-lightbox' id='sidebar-navigation-lightbox-search' on='lightboxClose:sidebar-navigation-close.show;lightboxOpen:sidebar-navigation-close.hide' layout='nodisplay' scrollable>";
 
-		echo "<p [text]=\"searchState.searchTemp == '' || searchState.searchTemp == ' ' || searchState.searchTemp == null ? 'Search term cannot be empty.' : 'Search results for: ' + searchState.searchTerm\">Search results</p>";
+		echo "<p [text]=\"searchState.searchTerm == '' || searchState.searchTerm == ' ' || searchState.searchTerm == null ? 'Search term cannot be empty.' : 'Search results for: ' + searchState.searchTerm\">Search results</p>";
 	
 		echo "<amp-list id='sidebar-navigation-lightbox-search-list' layout='container' width='800' height='800' [height]=\"edit-work-list.length * 1000\" items='.' max-items='100' binding='refresh' reset-on-refresh='always' [src]=\"'/api/search/?search=' + searchState.searchTerm\">";
 			echo "<p class='amp-list-fallback' [text]=\"'/api/search/?search=' + searchState.searchTerm\" fallback>No search results.</p>";
