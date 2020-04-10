1e76fd11436da66e3bb1abd9182ebcee1a5a6a30
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-09 22:49:12 -0700

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -168,7 +168,7 @@ function amp_header($title=null, $canonical=null) {
 				$tap_temp[] = "categories-list-popover-thread-". $header_backend_temp .".close";
 				endforeach;
 			$tap_temp[] = "categories-list-popover-thread-". $header_backend .".open";
-			echo "<div class='categories-popover-button' on='tap:". implode(",",$tap_temp) ."'>". ucfirst($header_frontend) ."</div>";
+			echo "<div class='categories-popover-button' role='button' tabindex='1' on='tap:". implode(",",$tap_temp) ."'>". ucfirst($header_frontend) ."</div>";
 			endforeach;
 	
 		echo "</amp-lightbox>";
