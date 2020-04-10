2820ac9282fa5a20ef3ca3cfeca56195d6de3d26
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-10 11:26:14 -0700

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -157,9 +157,9 @@ function amp_header($title=null, $canonical=null) {
 	
 		echo "<div id='sidebar-navigation-close' role='button' tabindex='0' on='tap:".implode(", ", $tap_temp)."' class='popover-close'>Back</div>";
 
-		echo "<amp-state id='searchState'><script type='application/json'>{'searchTerm': 'default' }</script></amp-state>";
-		echo "<input type='text' id='sidebar-navigation-search-input' on=\"change:AMP.pushState({searchState:{searchTerm: event.value}})\">";
-		echo "<div id='sidebar-navigation-search-button' role='button' tabindex='0' on=\"tap:sidebar-navigation-lightbox-search-list.refresh,sidebar-navigation-lightbox-search.open\">Search</div>";
+		echo "<amp-state id='searchState'><script type='application/json'>{'searchTemp': '','searchTerm': ''}</script></amp-state>";
+		echo "<input type='text' id='sidebar-navigation-search-input' on=\"change:AMP.setState({searchState:{searchTemp: event.value}})\">";
+		echo "<div id='sidebar-navigation-search-button' role='button' tabindex='0' on=\"tap:AMP.pushState({searchState:{searchTerm: searchState.searchTemp}}),sidebar-navigation-lightbox-search-list.refresh,sidebar-navigation-lightbox-search.open\">Search</div>";
 	
 		foreach ($header_array_temp as $header_backend => $header_frontend):
 			if (empty($type_counts_array[$header_backend]) && ($header_backend !== "main")): continue; endif;
