04e962ad69e793488bf7b2af2c2437c879adfc90
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-10 11:27:35 -0700

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -158,7 +158,7 @@ function amp_header($title=null, $canonical=null) {
 		echo "<div id='sidebar-navigation-close' role='button' tabindex='0' on='tap:".implode(", ", $tap_temp)."' class='popover-close'>Back</div>";
 
 		echo "<amp-state id='searchState'><script type='application/json'>{'searchTemp': '','searchTerm': ''}</script></amp-state>";
-		echo "<input type='text' id='sidebar-navigation-search-input' on=\"change:AMP.setState({searchState:{searchTemp: event.value}})\">";
+		echo "<input type='text' id='sidebar-navigation-search-input' on=\"input:AMP.setState({searchState:{searchTemp: event.value}})\">";
 		echo "<div id='sidebar-navigation-search-button' role='button' tabindex='0' on=\"tap:AMP.pushState({searchState:{searchTerm: searchState.searchTemp}}),sidebar-navigation-lightbox-search-list.refresh,sidebar-navigation-lightbox-search.open\">Search</div>";
 	
 		foreach ($header_array_temp as $header_backend => $header_frontend):
