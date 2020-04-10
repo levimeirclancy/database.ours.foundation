4e07a92f3127da439c827f7d8a0aa7fa9c58a275
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-09 22:53:22 -0700

    Update functions_content.php

--- functions_content.php
+++ functions_content.php
@@ -451,6 +451,8 @@ function body_process($body_incoming) {
 	$body_final .= implode(null, $image_lightbox_array);
 	$body_final = str_replace("\n", "<br>", $body_final);
 	$body_final = str_replace("><br>", ">", $body_final);
+	
+	str_replace("&#039;", "'", $body_final);
 
 	return $body_final; }
 
