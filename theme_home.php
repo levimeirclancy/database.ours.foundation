eb96d996af613237391fea757008328e3c9d8d10
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-04-14 15:06:41 -0700

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -5,7 +5,9 @@ $tap_temp = implode(",", [
 	"sidebar-navigation.open",
 	]);
 
-echo "<amp-img src='begin.png' id='begin-image' width='750' height='545' layout='intrinsic' alt='Begin' role='button' tabindex='0' on='tap:".$tap_temp."'></amp-img>";
+echo "<amp-img src='begin.png' id='begin-image' width='375' height='273' layout='intrinsic' alt='Begin' role='button' tabindex='0' on='tap:".$tap_temp."'></amp-img>";
+
+footer();
 
 echo "<h1>&#x2742;</h1>";
 
