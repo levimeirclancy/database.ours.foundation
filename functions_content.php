005ff4a1e95dd8ca95b484382cee623e4c249325
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-22 13:35:33 -0800

    Update functions_content.php

--- functions_content.php
+++ functions_content.php
@@ -239,7 +239,7 @@ function body_process($body_incoming) {
 		$link_string = str_replace("  ", " ", $link_string);
 		$link_string = trim($link_string);
 	
-		$body_incoming = str_replace("<amp-mathml ".$match_temp."amp-mathml>", $link_string, $body_incoming);
+		$body_incoming = str_replace("<amp-mathml".$match_temp."amp-mathml>", "<amp-mathml ".$link_string."amp-mathml>", $body_incoming);
 	
 		endforeach;
 		
