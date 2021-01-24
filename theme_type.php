31fba94542763a5d01bcd2abca620ba64d15b7a2
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-23 23:41:53 -0800

    Update theme_type.php

--- theme_type.php
+++ theme_type.php
@@ -70,13 +70,13 @@
 
 $entries_array  = print_row_loop ($page_temp, array_keys($information_array), []);
 
-echo "<ul>";
+echo "<ul class='entries-list'>";
 
 foreach ($entries_array as $entry_info):
 
 	$count_temp = 0;
 	if (!(is_int($entry_info['indent_count']))): $entry_info['indent_count'] = 0; endif;
-	echo "<li class='sidebar-navigation-item'><span class='categories-item-indent-wrapper'>";
+	echo "<li class='entries-list-item'><span class='categories-item-indent-wrapper'>";
 	while ($count_temp < $entry_info['indent_count']):
 		echo "<span class='categories-item-indent'></span>";
 		$count_temp++;
