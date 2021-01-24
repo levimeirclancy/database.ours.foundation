71e39d5edeac3cbe6d3149a7af3dbac1df5c854e
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-23 19:35:12 -0800

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -370,6 +370,6 @@ function notfound() {
 
 
 function footer() {
-	echo '<amp-mathml id="footer-formula" layout="container" data-formula="$$ תורה = תורה $$"></amp-mathml>';
+	echo '<amp-mathml id="footer-formula" layout="container" data-formula="\[ תורה = תורה \]"></amp-mathml>';
 	echo "</body></html>";
 	exit; } ?>
