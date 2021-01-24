2a12abebfe20dce8bf24c8c15d49c4784da84888
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-24 00:48:26 -0800

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -208,9 +208,9 @@ function amp_header($title=null, $canonical=null) {
 			echo "</ul></div></li>";
 
 		foreach ($header_array as $header_backend => $header_frontend):
-			echo "<li class='sidebar-navigation-item'><div class='sidebar-navigation-item-title' role='button' tabindex='0' on='tap:sidebar-navigation.changeToLayoutContainer();' amp-nested-submenu-open>". ucfirst($header_frontend) ."</div>";	
+			echo "<li class='sidebar-navigation-item'><div class='sidebar-navigation-item-title' role='button' tabindex='0' on='tap:sidebar-navigation.changeToLayoutContainer()' amp-nested-submenu-open>". ucfirst($header_frontend) ."</div>";	
 			echo "<div amp-nested-submenu><ul>";
-				echo "<li class='sidebar-navigation-close'><div class='sidebar-back' amp-nested-submenu-close>Back</li>";
+				echo "<li class='sidebar-navigation-close'><div class='sidebar-back' role='button' tabindex='0' on='tap:sidebar-navigation.changeToLayoutContainer()' amp-nested-submenu-close>Back</li>";
 				echo "<li class='sidebar-navigation-item'><a href='/".$header_backend."/'><span class='sidebar-navigation-item-title'>Type: ".$header_frontend."</span></a></li>";
 				$counter_temp = 0;
 				foreach($information_array as $entry_id => $entry_info):
