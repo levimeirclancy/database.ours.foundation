7ca64df1d0a5397a564b457fbb70f99af818a46a
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-24 00:26:29 -0800

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -212,7 +212,15 @@ function amp_header($title=null, $canonical=null) {
 			echo "<div class='sidebar-navigation-item' role='button' tabindex='0' on=\"tap:sidebar-navigation-lightbox-type-list.changeToLayoutContainer()\" amp-nested-submenu-open><div class='sidebar-navigation-item-title'>". ucfirst($header_frontend) ."</div></div>";	
 			echo "<div amp-nested-submenu><ul>";
 				echo "<li class='sidebar-navigation-close'><div class='sidebar-back' amp-nested-submenu-close>Back</li>";
-				echo "<div class='sidebar-navigation-item'><a href='/".$header_backend."/'>".$header_frontend."</a></li>";
+				echo "<li class='sidebar-navigation-item'><a href='/".$header_backend."/'><span class='sidebar-navigation-item-title'>Type: ".$header_frontend."</span></a></li>";
+				$counter_temp = 0;
+				foreach($information_id as $entry_id => $entry_info):
+					if ($entry_info['type'] !== $header_backend): continue; endif;
+					echo "<li class='sidebar-navigation-item'><a href='/". $entry_id . "/'>";
+					echo "<span class='sidebar-navigation-item-title'>".$entry_info['header']."</span></a></li>
+					$counter_temp++;
+					endforeach;
+				if ($counter_temp == 0): echo "<li><span class='sidebar-navigation-item-title'>No results.</span></li>"; endif;
 				echo "</ul></div>";
 			endforeach;
 
