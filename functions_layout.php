18ac0e63524059aa7d380a07c06fcaffc9e79bed
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-24 00:58:24 -0800

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -209,18 +209,18 @@ function amp_header($title=null, $canonical=null) {
 
 		foreach ($header_array as $header_backend => $header_frontend):
 			echo "<li class='sidebar-navigation-item'><div class='sidebar-navigation-item-title' role='button' tabindex='0' on='tap:sidebar-navigation.changeToLayoutContainer()' amp-nested-submenu-open>". ucfirst($header_frontend) ."</div>";	
-			echo "<div amp-nested-submenu>";
-				echo "<div class='sidebar-back' role='button' tabindex='0' on='tap:sidebar-navigation.changeToLayoutContainer()' amp-nested-submenu-close>Back</div>";
-				echo "<div class='sidebar-navigation-item'><a href='/".$header_backend."/'><span class='sidebar-navigation-item-title'>Type: ".$header_frontend."</span></a></div>";
+			echo "<div amp-nested-submenu><ul>";
+				echo "<li class='sidebar-navigation-close'><div class='sidebar-back' role='button' tabindex='0' on='tap:sidebar-navigation.changeToLayoutContainer()' amp-nested-submenu-close>Back</div></li>";
+				echo "<li class='sidebar-navigation-item'><a href='/".$header_backend."/'><span class='sidebar-navigation-item-title'>Type: ".$header_frontend."</span></a></li>";
 				$counter_temp = 0;
 				foreach($information_array as $entry_id => $entry_info):
 					if ($entry_info['type'] !== $header_backend): continue; endif;
-					echo "<div class='sidebar-navigation-item'><a href='/". $entry_id . "/'>";
-					echo "<span class='sidebar-navigation-item-title'>".$entry_info['header']."</span></a></div>";
+					echo "<li class='sidebar-navigation-item'><a href='/". $entry_id . "/'>";
+					echo "<span class='sidebar-navigation-item-title'>".$entry_info['header']."</span></a></li>";
 					$counter_temp++;
 					endforeach;
 				if ($counter_temp == 0): echo "<li class='sidebar-navigation-item'><span class='sidebar-navigation-item-title'>No results.</span></li>"; endif;
-				echo "</div></li>";
+				echo "</ul></div></li>";
 			endforeach;
 	
 			echo "</ul></amp-nested-menu>";
