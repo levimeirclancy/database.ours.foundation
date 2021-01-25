5451e1ce981d8266ef7072832fac2b97d3d542f7
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-24 17:38:45 -0800

    Update theme_type.php

--- theme_type.php
+++ theme_type.php
@@ -71,12 +71,12 @@
 
 	}
 
-echo "<div role='button' tabindex='0' id='entries-button-alphabetical' class='navigation-header-item' on='tap:entries-list-alphabetical.show,entries-list-hierarchical.hide,entries-button-alphabetical.hide,entries-button-hierarchical.show'>Alphabetical</div>";
+echo "<div role='button' tabindex='0' id='entries-button-alphabetical' class='navigation-header-item' on='tap:entries-list-alphabetical.show,entries-list-hierarchical.hide,entries-button-alphabetical.hide,entries-button-hierarchical.show' layout='container'>Alphabetical</div>";
 echo "<div role='button' tabindex='0' id='entries-button-hierarchical' class='navigation-header-item' on='tap:entries-list-alphabetical.hide,entries-list-hierarchical.show,entries-button-alphabetical.show,entries-button-hierarchical.hide' layout='nodisplay'>Hierarchical</div>";
 
 $entries_array  = print_row_loop ($page_temp, array_keys($information_array), []);
 
-echo "<ul id='entries-list-hierarchical'  class='entries-list'>";
+echo "<ul id='entries-list-hierarchical' class='entries-list' layout='container'>";
 
 	foreach ($entries_array as $entry_info):
 
@@ -96,6 +96,8 @@ echo "<ul id='entries-list-hierarchical'  class='entries-list'>";
 	echo "</ul>";
 
 echo "<ul id='entries-list-alphabetical' class='entries-list' layout='nodisplay'>";
+
+echo "test";
 
 	foreach ($information_array as $entry_info):
 
