4036071144f0f435ff5fecf212cb520b2f726720
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-25 22:58:39 -0800

    Update index.php

--- index.php
+++ index.php
@@ -265,8 +265,8 @@ if ($page_temp == "edit-xhr"):
 	$values_temp = [
 		"entry_id"		=> $_POST['entry_id'],
 		"type"			=> $_POST['type'],
-		"entry_published"	=> date("Y-m-d", (strtotime($_POST['entry_published'])+5)), // mySQL DATE column has format YYYY-MM-DD
-		"entry_updated"		=> date("Y-m-d H:i:s", time()), // mySQL DATETIME column has format YYYY-MM-DD hh:mm:ss
+		"date_published"	=> date("Y-m-d", (strtotime($_POST['date_published'])+5)), // mySQL DATE column has format YYYY-MM-DD
+		"date_updated"		=> date("Y-m-d H:i:s", time()), // mySQL DATETIME column has format YYYY-MM-DD hh:mm:ss
 		"name"			=> $_POST['name'],
 		"alternate_name"	=> $_POST['alternate_name'],
 		"summary" 		=> $_POST['summary'],
