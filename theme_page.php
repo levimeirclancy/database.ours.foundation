d6593b506b01a55354904eaf9b697dd0c50a0308
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-27 03:11:31 -0800

    Update theme_page.php

--- theme_page.php
+++ theme_page.php
@@ -109,6 +109,8 @@ echo "<div class='article-info' amp-fx='parallax' data-parallax-factor='1.2'>";
 			$grandparents_array = array_diff($grandparents_array, $information_array[$page_temp]['parents']['hierarchy']);
 			$grandparents_array = array_diff($grandparents_array, $information_array[$page_temp]['children']['hierarchy']);
 			$information_array[$page_temp]['grandparents']['hierarchy'] = $grandparents_array;
+
+print_r($grandparents_array);
 			endif;
 		endif;
 
