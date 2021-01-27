9177e4538ec9367d0e7d44ba22ffc626ac7240a1
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-27 11:49:35 -0800

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -207,7 +207,7 @@ function amp_header($title=null, $canonical=null) {
 		echo "<input type='text' id='search-input' required pattern=\".{1,}\" placeholder='...' on=\"input-throttled:AMP.setState({pageState:{searchTerm: event.value}}),sidebar-navigation-lightbox-search-list.changeToLayoutContainer()\">";
 		echo "<div id='search-submit' role='button' tabindex='0' on='tap:sidebar-navigation-lightbox-search-list.refresh,sidebar-navigation-lightbox-search-list.changeToLayoutContainer()'>Search</div>";
 	
-		echo "<ul><amp-list id='sidebar-navigation-lightbox-search-list' credentials='include' layout='responsive' width='800' height='300' max-items='100' binding='refresh' reset-on-refresh='always' items='searchResults' [src]=\"'/api/search/?search=' + pageState.searchTerm\">";
+		echo "<amp-list id='sidebar-navigation-lightbox-search-list' credentials='include' layout='responsive' width='800' height='300' max-items='100' binding='refresh' reset-on-refresh='always' items='searchResults' [src]=\"'/api/search/?search=' + pageState.searchTerm\">";
 
 		echo "<li class='sidebar-navigation-item' fallback><span class='sidebar-navigation-item-title'>No search results.</span></li>";
 		echo "<li class='sidebar-navigation-item' placeholder><span class='sidebar-navigation-item-title'>Loading search results...</span></li>";
@@ -216,7 +216,7 @@ function amp_header($title=null, $canonical=null) {
 			echo "<li class='sidebar-navigation-item'><a href='/{{entry_id}}/' ".$target_temp."><span class='sidebar-navigation-item-title' [text]='{{header}}'>{{header}}</span></a></li>";
 			echo "</template>";
 	
-		echo "</amp-list></ul>";
+		echo "</amp-list>";
 	
 		echo "</amp-sidebar>";
 	
