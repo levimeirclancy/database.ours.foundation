49b7be33b75f68d189810b4673b510845522a20b
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-01-28 20:28:34 -0800

    Update config_sample.php

--- config_sample.php
+++ config_sample.php
@@ -6,13 +6,8 @@ $database = "";
 
 $google_analytics_code = "";
 
-// the page id or username, e.g. reber.guide for https://facebook.com/reber.guide
-$messenger_bot = "";
-
-$page_access_token = "";
-
-$telegram_bot = "";
-
 $domain = "";
+
+$publisher = $author = "Foundation of Ours";
 
 $color = ""; ?>
