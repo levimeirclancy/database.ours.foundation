eed5ec6a861ffda26fbd419b5b743763e750aee5
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-05 20:22:50 -0800

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -1,2 +1,6 @@
-<? foreach ($information_array as $entry_id => $entry_info):
-  endforeach; ?>
+<? 
+$ordered_array = [];
+foreach ($information_array as $entry_id => $entry_info):
+   $ordered_array[$entry_id] = $entry_info['date_published'];
+
+	endforeach; ?>
