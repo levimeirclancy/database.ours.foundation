559841e5521a70aa9d61e76a9c953083424fe215
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-05 20:28:57 -0800

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -1,6 +1,34 @@
-<? 
-$ordered_array = [];
-foreach ($information_array as $entry_id => $entry_info):
-   $ordered_array[$entry_id] = $entry_info['date_published'];
+<?  if (empty($information_array)):
+	echo "<p>No entries yet.</p>";
+	footer();
+	endif;
 
-	endforeach; ?>
+$ordered_published_array = [];
+foreach ($information_array as $entry_id => $entry_info):
+	$ordered_published_array[$entry_id] = $entry_info['date_published'];
+	endforeach; 
+arsort($ordered_published_array);
+$ordered_published_array = array_slice($ordered_published_array, 0, 10);
+
+echo "<h2>Recently updated</h2>";
+echo "<ul>";
+foreach($ordered_published_array as $entry_id => $discard_info):
+	echo "<li><a href='/".$entry_id."/'>".$information_array[$entry_id]['header']."</a></li>";
+	endforeach;
+	echo "</ul>"; 
+
+$ordered_modified_array = [];
+foreach ($information_array as $entry_id => $entry_info):
+	$ordered_modified_array[$entry_id] = $entry_info['date_modified'];
+	endforeach; 
+arsort($ordered_modified_array);
+$ordered_array = array_slice($ordered_modified_array, 0, 10);
+
+echo "<h2>Recently modified</h2>";
+echo "<ul>";
+foreach($ordered_modified_array as $entry_id => $discard_info):
+	echo "<li><a href='/".$entry_id."/'>".$information_array[$entry_id]['header']."</a></li>";
+	endforeach;
+	echo "</ul>"; 
+
+?>
