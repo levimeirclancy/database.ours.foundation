61b3d363ef276481355618d85b406e106cb9e82d
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-05 20:35:23 -0800

    Update theme_home.php

--- theme_home.php
+++ theme_home.php
@@ -27,12 +27,13 @@ if (!(empty($ordered_published_array))):
 $ordered_updated_array = [];
 foreach ($information_array as $entry_id => $entry_info):
 	if (empty($entry_info['date_updated'])): continue; endif;
+	if (isset($ordered_published_array[$entry_id])): continue; endif;
 	$ordered_updated_array[$entry_id] = $entry_info['date_updated'];
 	endforeach; 
 if (!(empty($ordered_updated_array))):
 	arsort($ordered_updated_array);
-	$ordered_array = array_slice($ordered_modified_array, 0, 10);
-	echo "<h2>Recently updated</h2>";
+	$ordered_updated_array = array_slice($ordered_updated_array, 0, 10);
+	echo "<h2>Other updated posts</h2>";
 	echo "<ul>";
 		foreach($ordered_updated_array as $entry_id => $discard_info):
 		echo "<li><a href='/".$entry_id."/'>".$information_array[$entry_id]['header']."</a></li>";
