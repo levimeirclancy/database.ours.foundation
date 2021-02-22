8c004e8a6a9f6c4003c29041d7f30517881b34ac
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-22 13:21:49 -0800

    Update functions_layout.php

--- functions_layout.php
+++ functions_layout.php
@@ -189,7 +189,7 @@ function amp_header($title=null, $canonical=null) {
 		echo "<li><a href='/' ".$target_temp."><p>".$publisher."</p></a></li>";
 		
 		foreach ($site_info['category_array'] as $header_backend => $header_frontend):
-			echo "<li><a href='/".$header_backend."/' ".$target_temp."><p>". ucfirst($header_frontend) ."</p></a></li>";
+			echo "<li><a href='/".$header_backend."/' ".$target_temp.">". ucfirst($header_frontend) ."</a></li>";
 			endforeach;
 	
 		echo "</ul>";
@@ -209,7 +209,7 @@ function amp_header($title=null, $canonical=null) {
 		echo "<amp-list id='sidebar-navigation-lightbox-search-list' credentials='include' layout='responsive' width='800' height='300' max-items='100' binding='refresh' reset-on-refresh='always' items='searchResults' [src]=\"'/api/search/?search=' + pageState.searchTerm\">";
 
 		echo "<li placeholder><p>Loading search results...</p></li>";
-		echo "<li fallback><p>No search results.</p>";
+		echo "<li fallback><p>No search results.</p></li>";
 
 		echo "<template type='amp-mustache'>";
 			echo "<li><a href='/{{entry_id}}/' ".$target_temp."><p>{{header}}</p></a></li>";
