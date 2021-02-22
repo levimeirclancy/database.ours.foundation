dc433f8a9dc554d24e76c7f5e4ce29fecd1f842a
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-22 10:57:23 -0800

    Update style.php

--- style.php
+++ style.php
@@ -942,8 +942,8 @@ $style_array = [
 	"css_tags" => "li",
 	"css_contents" =>
 		[
-		"padding"		=> "12px 5px 5px 6px",
-		"margin"		=> "-7px 0 0 0",
+		"padding"		=> "10px 5px 5px 6px",
+		"margin"		=> "-5px 0 0 0",
 		"border-left"		=> "1px solid ".output_rgba($font_color, 0.2),
 		"border-bottom"		=> "1px dotted ".output_rgba($font_color, 0.2),
 		"position"		=> "relative",
