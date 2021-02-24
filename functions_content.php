7c6ecc2b304a11e84db34266bfc852bcb5b9c723
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-24 14:46:06 -0800

    Update functions_content.php

--- functions_content.php
+++ functions_content.php
@@ -353,10 +353,10 @@ function body_process($body_incoming) {
 				endwhile;
 	
 			$specifier_temp = null;
-			if (strpos(strtoupper($digestion_temp), "UL".$list_delimiter) == ($indent_current_temp*3)):
+			if (strpos($digestion_temp, "UL".$list_delimiter) == ($indent_current_temp*3)):
 				$specifier_temp = "ul";
 				$digestion_temp = trim(substr($digestion_temp, 5));
-			elseif (strpos(strtoupper($digestion_temp), "OL".$list_delimiter) == ($indent_current_temp*3)):
+			elseif (strpos($digestion_temp, "OL".$list_delimiter) == ($indent_current_temp*3)):
 				$specifier_temp = "ol";
 				$digestion_temp = trim(substr($digestion_temp, 5));
 				endif;
