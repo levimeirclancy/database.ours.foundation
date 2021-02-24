70576ee9138a5a611ef14dd808b62f839bce98ca
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-23 19:17:13 -0800

    Update admin_page.php

--- admin_page.php
+++ admin_page.php
@@ -134,7 +134,7 @@ echo "<amp-lightbox id='delete-popover' layout='nodisplay'>";
 
 	echo "</amp-lightbox>";
 
-echo "<span class='sidebar-inputs-button' role='button' tabindex='0' on='tap:sidebar-inputs.open'>☰</span>";
+echo "<span id='sidebar-inputs-button' role='button' tabindex='0' on='tap:sidebar-inputs.open'>☰</span>";
 
 echo "<form action-xhr='/edit-xhr/' method='post' class='admin-page-form' id='save' on=\"
 		submit:
