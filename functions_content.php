9e396d6be3291c33c2ab8abafb025cebb09a737b
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-24 13:28:04 -0800

    Update functions_content.php

--- functions_content.php
+++ functions_content.php
@@ -354,7 +354,7 @@ function body_process($body_incoming) {
 				$replace_temp .= "</li><li>".$indent_position_temp."|.$indent_current_temp."|";
 			elseif ($indent_position_temp < $indent_current_temp):
 				while ($indent_position_temp < $indent_current_temp):
-					$replace_temp .= "<ul><li>".$indent_position_temp."|.$indent_current_temp."|";
+					$replace_temp .= "<ul><li>".$indent_position_temp."|".$indent_current_temp."|";
 					$indent_position_temp++;
 					endwhile;
 			elseif ($indent_position_temp > $indent_current_temp):
@@ -362,7 +362,7 @@ function body_process($body_incoming) {
 					$replace_temp .= "</li></ul></li>";
 					$indent_current_temp++;
 					endwhile;
-				$replace_temp .= "<li>".$indent_position_temp."|.$indent_current_temp."|";
+				$replace_temp .= "<li>".$indent_position_temp."|".$indent_current_temp."|";
 				endif;
 	
 			$indent_position_temp = $indent_current_temp;
