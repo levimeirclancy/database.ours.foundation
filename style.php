4b009cd10717a73ec57fd16e53716a00c1604b15
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-26 01:11:28 -0800

    Update style.php

--- style.php
+++ style.php
@@ -573,7 +573,7 @@ $style_array = [
 		],
 	"css_contents" =>
 		[
-		"display"		=> "block",
+		"display"		=> "inline-block",
 		"margin"		=> "5px 100px 5px 20px",
 		"padding"		=> "8px",
 		"width"			=> output_width($default_width),
