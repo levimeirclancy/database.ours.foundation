34b8d520553c9481a8b865cc83e0b3b074449396
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-26 22:48:38 -0800

    Update hotellings-t-square.php

--- calculators/hotellings-t-square.php
+++ calculators/hotellings-t-square.php
@@ -91,7 +91,7 @@ if (count($hypothetical_mean) !== count($sample_mean)): error("Hypothetical mean
 
 $mean_difference = [];
 foreach ($hypothetical_mean as $key_temp => $value_temp):
-	$mean_difference[] = ( $sample_mean[$key_temp] - $hypothetical_mean[$key_temp] );
+	$mean_difference[] = [ $sample_mean[$key_temp][0] - $hypothetical_mean[$key_temp][0] ];
 	endforeach;
 
 foreach( $variance_covariance as $row_temp):
