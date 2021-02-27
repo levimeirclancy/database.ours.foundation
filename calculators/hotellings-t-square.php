dd2b6c6499ec22208fddcaf7eabb3912812511a8
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-02-26 23:57:38 -0800

    Update hotellings-t-square.php

--- calculators/hotellings-t-square.php
+++ calculators/hotellings-t-square.php
@@ -198,10 +198,11 @@ foreach ($hypothetical_mean as $key_temp => $value_temp):
 	$mean_difference[] = [ $sample_mean[$key_temp][0] - $hypothetical_mean[$key_temp][0] ];
 	endforeach;
 
-print_r($mean_difference); exit;
-
 // Calculate inverse of the variance-covariance matrix,
 $variance_covariance_inverse = inverse_matrix($variance_covariance);
+
+print_r($variance_covariance_inverse); exit;
+
 
 // print_r($variance_covariance_inverse);
 
