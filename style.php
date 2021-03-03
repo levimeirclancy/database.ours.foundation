41bda7155d724f9d86f3a2906f56902adb83be62
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-03-03 13:48:32 -0800

    Update style.php

--- style.php
+++ style.php
@@ -416,7 +416,7 @@ $style_array = [
 		"border-style"		=> "dotted",
 		"border-color"		=> output_rgba($font_color, 1),
 		"border-radius"		=> "15px",
-		"padding"		=> "15px 10px 5px",
+		"padding"		=> "15px 10px",
 		"max-width"		=> output_width($default_width,-150),
 		], ],
 		
