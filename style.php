7a0f5cc156e3c04e532032c0dd77ef25c836d27a
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-03-03 13:01:15 -0800

    Update style.php

--- style.php
+++ style.php
@@ -437,7 +437,7 @@ $style_array = [
 	"css_contents" =>
 		[
 		"box-sizing"		=> "border-box",
-//		"padding"		=> "10px",
+		"padding"		=> "20px 0",
 		"margin"		=> "0",
 		"border"		=> "1px solid ".output_rgba($font_color, 1),
 		], ],
