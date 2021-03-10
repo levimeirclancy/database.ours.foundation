c2e0b1747e421409a2678a1c39892110fcd8680d
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-03-10 14:06:54 -0800

    Update style.php

--- style.php
+++ style.php
@@ -1126,7 +1126,7 @@ $style_array = [
 		"max-width"		=> "100%",
 		"height"		=> output_width($site_info['dimensions']['width']*.7),
 		"max-height"		=> "70%",
-		"margin"		=> "20px",
+//		"margin"		=> "20px",
 		"box-sizing"		=> "border-box",
 		"text-align"		=> "left",
 		], ],
