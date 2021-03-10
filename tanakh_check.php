9cac23f2569fe2d305893ccc633e4c80e6646a19
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-03-09 16:54:06 -0800

    Update tanakh_check.php

--- tanakh_check.php
+++ tanakh_check.php
@@ -70,89 +70,89 @@
 			"50" => 26,
 			], ],
 
-		"exodus" =>
+	"exodus" =>
+		[
+		"name"			=> "שמות Exodus",
+		"chapters-verses"	=>
 			[
-			"name"			=> "שמות Exodus",
-			"chapters-verses"	=>
-				[
-				"1" => null,
-				"2" => null,
-				"3" => null,
-				"4" => null,
-				"5" => null,
-				"6" => null,
-				"7" => null,
-				"8" => null,
-				"9" => null,
-				"10" => null,
-				"11" => null,
-				"12" => null,
-				"13" => null,
-				"14" => null,
-				"15" => null,
-				"16" => null,
-				"17" => null,
-				"18" => null,
-				"19" => null,
-				"20" => null,
-				"21" => null,
-				"22" => null,
-				"23" => null,
-				"24" => null,
-				"25" => null,
-				"25" => null,
-				"26" => null,
-				"27" => null,
-				"28" => null,
-				"29" => null,
-				"30" => null,
-				"31" => null,
-				"32" => null,
-				"33" => null,
-				"34" => null,
-				"35" => null,
-				"36" => null,
-				"37" => null,
-				"38" => null,
-				"39" => null,
-				"40" => null,
-				], ],
+			"1" => 22,
+			"2" => 25,
+			"3" => 2,
+			"4" => 31,
+			"5" => null,
+			"6" => null,
+			"7" => null,
+			"8" => null,
+			"9" => null,
+			"10" => null,
+			"11" => null,
+			"12" => null,
+			"13" => null,
+			"14" => null,
+			"15" => null,
+			"16" => null,
+			"17" => null,
+			"18" => null,
+			"19" => null,
+			"20" => null,
+			"21" => null,
+			"22" => null,
+			"23" => null,
+			"24" => null,
+			"25" => null,
+			"25" => null,
+			"26" => null,
+			"27" => null,
+			"28" => null,
+			"29" => null,
+			"30" => null,
+			"31" => null,
+			"32" => null,
+			"33" => null,
+			"34" => null,
+			"35" => null,
+			"36" => null,
+			"37" => null,
+			"38" => null,
+			"39" => null,
+			"40" => null,
+			], ],
 
-		"leviticus" => 
+	"leviticus" => 
+		[
+		"name"			=> "ויקרא Leviticus",
+		"chapters-verses"	=>
 			[
-			"name"			=> "ויקרא Leviticus",
-			"chapters-verses"	=>
-				[
-				"1" => null,
-				"2" => null,
-				"3" => null,
-				"4" => null,
-				"5" => null,
-				"6" => null,
-				"7" => null,
-				"8" => null,
-				"9" => null,
-				"10" => null,
-				"11" => null,
-				"12" => null,
-				"13" => null,
-				"14" => null,
-				"15" => null,
-				"16" => null,
-				"17" => null,
-				"18" => null,
-				"19" => null,
-				"20" => null,
-				"21" => null,
-				"22" => null,
-				"23" => null,
-				"24" => null,
-				"25" => null,
-				"25" => null,
-				"26" => null,
-				"27" => null,
-				], ],
-		
+			"1" => null,
+			"2" => null,
+			"3" => null,
+			"4" => null,
+			"5" => null,
+			"6" => null,
+			"7" => null,
+			"8" => null,
+			"9" => null,
+			"10" => null,
+			"11" => null,
+			"12" => null,
+			"13" => null,
+			"14" => null,
+			"15" => null,
+			"16" => null,
+			"17" => null,
+			"18" => null,
+			"19" => null,
+			"20" => null,
+			"21" => null,
+			"22" => null,
+			"23" => null,
+			"24" => null,
+			"25" => null,
+			"25" => null,
+			"26" => null,
+			"27" => null,
+			], ],
+
 		"numbers" => 
 			[
 			"name"			=> "במדבר Numbers",
@@ -311,7 +311,7 @@
 				"4" => null,
 				], ],
 
-		"i samuel" => 
+		"1samuel" => 
 			[
 			"name"			=> "שמואל Samuel I",
 			"chapters-verses"	=>
@@ -350,7 +350,7 @@
 				"31" => null,
 				], ],
 
-		"ii samuel" => 
+		"2samuel" => 
 			[
 			"name"			=> "שמואל Samuel II",
 			"chapters-verses"	=>
@@ -716,3 +716,4 @@ function tanakh_check($contents_string, $book_given=null, $chapter_given=null, $
 	return $return_string;
 
 	} ?>
+ 
