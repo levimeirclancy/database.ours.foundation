6039f2085ad2aa4b76eadc74987c57dccc231082
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-03-10 13:43:34 -0800

    Update functions_content.php

--- functions_content.php
+++ functions_content.php
@@ -594,10 +594,16 @@ function body_process($body_incoming) {
 //		$image_string .= "</figure>";
 
 		if ($link_check == 1):
+	
+			$image_string .= "<figure>";
+	
 			$image_string .= "<div class='amp-img-large-wrapper'>";
-			$image_string .= "<figure><amp-img src='".$image_url."' role='button' tabindex='1' layout='fill' class='amp-img-large'></amp-img>";
+			$image_string .= "<amp-img src='".$image_url."' role='button' tabindex='1' layout='fill' class='amp-img-large'></amp-img>";
+			$image_string .= "</div>";
+
 			$image_string .= "<figcaption>". mb_substr(strip_tags($file_description),0,200) ."</figcaption>";
-			$image_string .= "</figure></div>";
+
+			$image_string .= "</figure>";
 			endif;
 
 //		$media_info = nesty_media($temp_array[0]);
