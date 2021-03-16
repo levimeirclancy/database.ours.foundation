a09cff6a316920df8b3dc9b458009502646c5605
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2021-03-16 11:03:38 -0700

    Update style.php

--- style.php
+++ style.php
@@ -269,7 +269,7 @@ $style_array = [
 		"line-height"		=> "140%",
 		"text-align"		=> "left",
 		"font-weight"		=> "400",
-		"text-shadow"		=> "2px 2px 15px -0 ".output_rgba($site_info['colors']['font'], 0.5),
+		"text-shadow"		=> "2px 2px 15px  ".output_rgba($site_info['colors']['font'], 1),
 		], ],
 	
 	[
